7ebb22677d776435a5b437aa59188b3c34944729
wenoki <i@dec31.net>
2013-11-13 12:44:34 +0900

    うーん

--- app.rb
+++ app.rb
@@ -14,8 +14,7 @@ AWS.config(
 )
 
 class ShinuDotCom < Sinatra::Base
-#  DomainName = SimpleIDN.to_ascii "死ぬ.com"
-  DomainName = "localhost"
+  DomainName = SimpleIDN.to_ascii "死ぬ.com"
 
   log = Logger.new STDOUT
   STDOUT.sync = true
@@ -24,33 +23,31 @@ class ShinuDotCom < Sinatra::Base
     register Sinatra::Reloader
   end
 
-  helpers do
-    def current_subdomain
-      request.host.match(/\A(.*)#{DomainName}\z/)[1].chop
-    end
-  end
-
   error do
     redirect SimpleIDN.to_ascii("エラーが発生したので.") + DomainName
   end
 
   get "/" do
-    Subdomain.find current_subdomain
-    # @subdomain = Subdomain.find_or_create request.host.match(/\A(.*)#{DomainName}\z/)[1].chop
-
-
-#    slim :application
+    @domain = Domain.find_or_create request.host
+    @domain.view_count
+    slim :application
+    @domain.count_up
   end
 end
 
-class Subdomain
+class Domain
   attr_accessor :name, :view_count, :since
   HostedZoneId = ENV["HOSTED_ZONE_ID"]
 
-  def initialize name, view_count = 0, since = DateTime.now
+  def initialize name, view_count = 1, since = DateTime.now
     self.name = name
     self.view_count = view_count
     self.since = since
+  end
+
+  def count_up
+    self.view_count += 1
+    self.save
   end
 
   def decoded_name
@@ -58,23 +55,40 @@ class Subdomain
   end
 
   def self.find name
-    "oh" if record_set(name).exists?
+    self.new(
+      name,
+      rrset(name).resource_records[0][:value].to_i,
+      DateTime.parse(rrset(name).resource_records[1][:value]),
+    )
   end
 
-  def self.create
-
+  def self.create name
+    self.new name
   end
 
-  def self.find_or_create
+  def self.find_or_create name
+    if rrset(name).exists?
+      find name
+    else
+      create name
+    end
   end
 
-  def self.record_set name
-    AWS::Route53::HostedZone.new(HostedZoneId).rrsets[[name, ShinuDotCom::DomainName, ""].compact.join(?.), "TXT"]
+  def self.rrset name
+    AWS::Route53::HostedZone.new(HostedZoneId).rrsets[name + ?., "TXT"]
+  end
+
+  private
+  def save
+    rrset = self.class.rrset(self.name)
+    rrset.resource_records[0][:value] = %("#{self.view_count}")
+    rrset.resource_records[1][:value] = %("#{self.since.to_s}")
+    rrset.update
   end
 end
 
 class Integer
   def with_delimiter
-    self.to_s.reverse.gsub(/(\d{3})(?=\d)/, "\1,").reverse
+    self.to_s.reverse.gsub(/(\d{3})(?=\d)/, "\\1,").reverse
   end
 end
